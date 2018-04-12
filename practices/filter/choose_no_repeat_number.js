94b4ffea9776edaf825a6668a5213ce2184a1aca
ChersieGao <1204157511@qq.com>
2018-04-12 16:28:37 +0800

    collection

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -4,16 +4,15 @@ function choose_no_repeat_number(collection) {
 
   //在这里写入代码
   var reslult = [];
-  var flag = 0;
   for(var i = 0;i < collection.length; i++){
-    for(var j = 0;j < collection.length; j++){
-      if(i == j) continue;
-      else {
-        if (collection[i] == collection[j]) flag = 1;
+    for(var j = i+1; j < collection.length; j++){
+      if(collection[i] === collection[j]){
+        j = ++i;
       }
-      if(!flag) reslult.push(collection[i]);
     }
-  }
+    reslult.push(collection[i]);
+  } // 去重复
+
   return reslult;
 }
 
